f6c133def718403bb0e42da9327b8e2868e106cc
Guillaume <gui.simon@gmail.com>
2019-11-23 17:00:53 +0100

    simple script for convert json to csv using pandas

---
+++ script/json2csv.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+'''
+Convert a json file into a csv file using pandas.
+
+Givin researcher.json as
+
+
+[
+    {
+	"first_name": "Pieter",
+	"last_name": "Abbeel",
+	"homepage":"https://people.eecs.berkeley.edu/~pabbeel",
+	"linkedin":"https://www.linkedin.com/in/pieterabbeel",
+	"fields": ["deep-learning","reinforcement-learning","robotics"]
+    },
+    {
+	"first_name": "Geoffrey",
+	"last_name": "Hinton",
+	"homepage":"https://www.cs.toronto.edu/~hinton",
+	"wikipedia":"https://www.cs.toronto.edu/~hinton",
+	"fields": ["deep-learning"]
+    }    
+]
+
+will output researcher.csv as
+
+,first_name,last_name,homepage,linkedin,fields,wikipedia
+0,Pieter,Abbeel,https://people.eecs.berkeley.edu/~pabbeel,https://www.linkedin.com/in/pieterabbeel,"['deep-learning', 'reinforcement-learning', 'robotics']",
+1,Geoffrey,Hinton,https://www.cs.toronto.edu/~hinton,,['deep-learning'],https://www.cs.toronto.edu/~hinton
+
+'''
+import sys
+import pandas as pd
+pin = sys.argv[1]
+pout = pin.replace('.json', '.csv')
+df = pd.read_json(pin)
+df.to_csv(pout)    

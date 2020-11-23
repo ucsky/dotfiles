2e38ae88d0a8ccf023404668ca674e06c539b8f0
Guillaume SIMON <gui.simon@gmail.com>
2020-11-23 10:03:40 +0100

    add vscode

--- script/bash/post-install-update-dotfiles.bash
+++ script/bash/post-install-update-dotfiles.bash
@@ -92,6 +92,17 @@ install_dbeaver () {
 	echo "dbeaver ALREADY INSTALLED."
     fi
 }
+# Editor
+install_vscode () {
+    if [ -z $(command -v code) ];then
+	wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+	sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+	sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+	sudo apt-get install apt-transport-https
+	sudo apt-get update
+	sudo apt-get install code
+    fi
+}
 
 ######################################################
 # main
@@ -128,3 +139,6 @@ check_install_apt_pkg texlive-full
 
 # DB management software
 install_dbeaver
+
+# Editors
+install_vscode

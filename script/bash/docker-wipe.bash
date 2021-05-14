4698a13abd164d4b34906e19617361d557882f94
Guillaume SIMON <gui.simon@gmail.com>
2021-05-14 15:13:40 +0200

    add docker cleanup script

---
+++ script/bash/docker-wipe.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "Stop all containers"
+docker stop `docker ps -qa`
+
+echo "Remove all containers"
+docker rm `docker ps -qa`
+
+echo "Remove all images"
+docker rmi -f `docker images -qa `
+
+echo "Remove all volumes"
+docker volume rm $(docker volume ls -qf)
+
+echo "Remove all networks"
+docker network rm `docker network ls -q`
+
+echo "Your installation should now be all fresh and clean."
+
+echo "The following commands should not output any items:"
+docker ps -a
+docker images -a 
+docker volume ls
+
+echo "The following command show only show the default networks:"
+docker network ls

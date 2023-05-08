eaf6ada5df434faec17331e82937ff802f772b9e
Guillaume <gui.simon@gmail.com>
2023-05-08 11:51:57 +0200

    add usage

--- script/bash/docker-checksize.bash
+++ script/bash/docker-checksize.bash
@@ -1,5 +1,10 @@
 #!/bin/bash -e
-
+#
+# Usage: sudo ./docker-checksize.bash
+#
+# Lists the disk usage of all the files and directories within
+# the /var/lib/docker directory in a human-readable format.
+# 
 # change directory to /var/lib/docker
 pushd /var/lib/docker > /dev/null
 

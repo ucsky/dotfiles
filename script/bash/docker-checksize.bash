bf941e052236d7d5694775478d292cc2a19c50d9
Guillaume <gui.simon@gmail.com>
2023-05-08 11:42:33 +0200

    remove sudo and add comments

--- script/bash/docker-checksize.bash
+++ script/bash/docker-checksize.bash
@@ -1,7 +1,13 @@
 #!/bin/bash -e
-pushd /var/lib/docker > /dev/null
-for i in `sudo ls`;do
-    sudo du -sh $i;
-done
-popd > /dev/null
 
+# change directory to /var/lib/docker
+pushd /var/lib/docker > /dev/null
+
+# loop through each file and directory in /var/lib/docker
+for i in `sudo ls`; do
+    # display the disk usage of the current file or directory in a human-readable format
+    du -sh $i
+done
+
+# change back to the previous directory
+popd > /dev/null

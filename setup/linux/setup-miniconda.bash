e018aa5f20b9076c33f0dd4b2f784fe71cd27d73
Guillaume SIMON <gsimon@miratlas.com>
2023-10-23 13:53:31 +0000

    use ROOT_MINICONDA env var in order to bypass default value

--- setup/linux/setup-miniconda.bash
+++ setup/linux/setup-miniconda.bash
@@ -1,23 +1,24 @@
 #!/bin/bash
-test ! -d ~/.miniconda3 \
-    && mkdir -p ~/.miniconda3 \
+ROOT_MINICONDA="${ROOT_MINICONDA:-~/.miniconda3}"
+test ! -d ${ROOT_MINICONDA} \
+    && mkdir -p ${ROOT_MINICONDA} \
 	|| true
-test ! -f ~/.miniconda3/miniconda.sh \
+test ! -f ${ROOT_MINICONDA}/miniconda.sh \
     && wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh \
-	    -O ~/.miniconda3/miniconda.sh \
+	    -O ${ROOT_MINICONDA}/miniconda.sh \
 	|| true
-bash ~/.miniconda3/miniconda.sh -b -u -p ~/.miniconda3
+bash ${ROOT_MINICONDA}/miniconda.sh -b -u -p ${ROOT_MINICONDA}
 
 test ! -d $HOME/activate && mkdir $HOME/activate || true
 cat <<EOF > $HOME/activate/miniconda3 
-__conda_setup="\$('/home/${USER}/.miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+__conda_setup="\$('${ROOT_MINICONDA}/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 if [ \$? -eq 0 ]; then
     eval "\$__conda_setup"
 else
-    if [ -f "/home/${USER}/.miniconda3/etc/profile.d/conda.sh" ]; then
-        . "/home/${USER}/.miniconda3/etc/profile.d/conda.sh"
+    if [ -f "${ROOT_MINICONDA}/etc/profile.d/conda.sh" ]; then
+        . "${ROOT_MINICONDA}/etc/profile.d/conda.sh"
     else
-        export PATH="/home/${USER}/.miniconda3/bin:\$PATH"
+        export PATH="${ROOT_MINICONDA}/bin:\$PATH"
     fi
 fi
 unset __conda_setup

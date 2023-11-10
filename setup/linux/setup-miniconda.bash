753d84ea17b4cce2dffeccedbc326ae714e4116c
Guil <gsimon@miratlas.com>
2023-11-10 19:29:37 +0100

    simplify

--- setup/linux/setup-miniconda.bash
+++ setup/linux/setup-miniconda.bash
@@ -1,12 +1,9 @@
 #!/bin/bash
 ROOT_MINICONDA="${ROOT_MINICONDA:-$HOME/.miniconda3}"
-test ! -d ${ROOT_MINICONDA} \
-    && mkdir -p ${ROOT_MINICONDA} \
-	|| true
-test ! -f ${ROOT_MINICONDA}/miniconda.sh \
-    && wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh \
-	    -O ${ROOT_MINICONDA}/miniconda.sh \
-	|| true
-bash ${ROOT_MINICONDA}/miniconda.sh -b -u -p ${ROOT_MINICONDA}
+test -d ${ROOT_MINICONDA} || mkdir -p ${ROOT_MINICONDA}
+test -f ${ROOT_MINICONDA}/install.sh \
+    || wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh \
+	    -O ${ROOT_MINICONDA}/install.sh
+bash ${ROOT_MINICONDA}/install.sh -b -u -p ${ROOT_MINICONDA}
 ${ROOT_MINICONDA}/bin/conda init
 ${ROOT_MINICONDA}/bin/conda config --set auto_activate_base false

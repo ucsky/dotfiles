c9080e34101121d25c82ce37f81a2b66732fd9c0
Guil <gsimon@miratlas.com>
2023-11-10 19:26:00 +0100

    fix root location, deactivate base by default

--- setup/linux/setup-miniconda.bash
+++ setup/linux/setup-miniconda.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-ROOT_MINICONDA="${ROOT_MINICONDA:-~/.miniconda3}"
+ROOT_MINICONDA="${ROOT_MINICONDA:-$HOME/.miniconda3}"
 test ! -d ${ROOT_MINICONDA} \
     && mkdir -p ${ROOT_MINICONDA} \
 	|| true
@@ -8,18 +8,5 @@ test ! -f ${ROOT_MINICONDA}/miniconda.sh \
 	    -O ${ROOT_MINICONDA}/miniconda.sh \
 	|| true
 bash ${ROOT_MINICONDA}/miniconda.sh -b -u -p ${ROOT_MINICONDA}
-
-test ! -d $HOME/activate && mkdir $HOME/activate || true
-cat <<EOF > $HOME/activate/miniconda3 
-__conda_setup="\$('${ROOT_MINICONDA}/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-if [ \$? -eq 0 ]; then
-    eval "\$__conda_setup"
-else
-    if [ -f "${ROOT_MINICONDA}/etc/profile.d/conda.sh" ]; then
-        . "${ROOT_MINICONDA}/etc/profile.d/conda.sh"
-    else
-        export PATH="${ROOT_MINICONDA}/bin:\$PATH"
-    fi
-fi
-unset __conda_setup
-EOF
+${ROOT_MINICONDA}/bin/conda init
+${ROOT_MINICONDA}/bin/conda config --set auto_activate_base false
